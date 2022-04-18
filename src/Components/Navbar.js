fd86004a71b0982e0a4c776caab3b8f6296a9a2e
pumba <nompumelelomasilela23@gmail.com>
2022-04-18 17:50:59 +0200

    things

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from "../Assets/logo.png";
+import Logo from "../Assets/logo.png";
 import { Link } from 'react-router-dom';
 
 export default class Navbar  extends Component {
@@ -7,7 +7,7 @@ export default class Navbar  extends Component {
     return (
        <div classname ="Navbar" > 
             <div classname="leftside">
-              <img src={logo}/>
+              <img src={Logo}/>
           </div>
             <div classname="rightside">
             </div>
